381ca08a0247fe49b73bac8ae46154eca371e33f
Erwan Vallée <erwanp.vallee@gmail.com>
2024-02-13 15:45:25 +0100

    init

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import styles from './Header.module.scss';
+import { NavLink } from "react-router-dom";
+
+function Header() {
+  return (
+    <header className={`${styles.header} d-flex flex-row align-items-center`}>
+      <div className="flex-fill">
+        <NavLink to="/">
+          <strong>JWT</strong>
+        </NavLink>
+      </div>
+      <ul className={styles.headerList}>
+          <NavLink to="signup" className="mr-15">
+            Inscription
+          </NavLink>
+      </ul>
+    </header>
+  );
+}
+
+export default Header;
---
+++ client/src/router.jsx
@@ -0,0 +1,25 @@
+/* eslint-disable react-refresh/only-export-components */
+
+import { lazy } from "react";
+import { createBrowserRouter } from "react-router-dom";
+import App from "./App";
+
+const Homepage = lazy(() => import("./pages/Homepage/Homepage"));
+const Signup = lazy(() => import("./pages/Signup/Signup"));
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Homepage />,
+      },
+      {
+        path: "signup",
+        element: <Signup />,
+      },
+    ],
+  },
+]);
